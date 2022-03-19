423e86d04f17702dee48b4c1b62ad5c885646866
Aleksandr Karev <w88190@gmail.com>
2022-03-19 16:52:35 +0700

    completed project (2 iteration)

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,18 @@
-
-const popup = document.querySelector(".popup");
-const openPopup = document.querySelector(".profile__edit-button");
-const closePopup = popup.querySelector(".popup__close");
-const formElement = document.querySelector(".popup__form");
-const nameInput = formElement.querySelector(".popup__form-name");
-const jobInput = formElement.querySelector(".popup__form-about");
-const profileName = document.querySelector(".profile__title");
-const profileJob = document.querySelector(".profile__subtitle");
+const popup = document.querySelector('.popup');
+const popupEditButton = document.querySelector('.profile__edit-button');
+const popupCloseButton = popup.querySelector('.popup__close-button');
+const formElement = document.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_about');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
 
 function togglePopup(event) {
-    popup.classList.toggle("popup_opened");
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-}
-
-function closePopupArea(event) {                  /* Функция позволяющая закрывать popup не только по кнопке "Close"*/
-    if (event.target === event.currentTarget) {
-        togglePopup();
+    if (!popup.classList.contains('popup_opened')) {
+        nameInput.value = profileName.textContent;
+        jobInput.value = profileJob.textContent;
     }
+    popup.classList.toggle('popup_opened');
 }
 
 function formSubmitHandler (evt) {
@@ -27,7 +22,6 @@ function formSubmitHandler (evt) {
     togglePopup();
 }
 
-popup.addEventListener("click", closePopupArea);
-openPopup.addEventListener("click", togglePopup);
-closePopup.addEventListener("click", togglePopup);
+popupEditButton.addEventListener("click", togglePopup);
+popupCloseButton.addEventListener("click", togglePopup);
 formElement.addEventListener("submit", formSubmitHandler);
