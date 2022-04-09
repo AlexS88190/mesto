56cca16bc8f34a405feb6549a885714ee368f63f
Aleksandr Karev <w88190@gmail.com>
2022-04-09 20:46:25 +0700

    валидация popup profile

--- scripts/script.js
+++ scripts/script.js
@@ -143,3 +143,50 @@ popupList.forEach(popup => {
 
 document.addEventListener('keydown', closePopup);
 
+
+
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add('popup__input_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('popup__input_type_error');
+    errorElement.classList.remove('popup__input-error_active');
+    errorElement.textContent = '';
+}
+
+const isValid = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+        hideInputError(formElement, inputElement);
+    }
+}
+
+const setEventListeners = (formElement) => {
+
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    //const buttonElement = formElement.querySelector('.popup__save-button');
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', () => {
+            isValid(formElement, inputElement);
+        });
+    });
+}
+
+const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll('.popup__form'));
+    formList.forEach((formElement) => {
+        formElement.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+        setEventListeners(formElement);
+    });
+}
+
+enableValidation();
