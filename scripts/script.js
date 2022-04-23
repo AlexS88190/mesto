a6024d67443369ebeaeb9f81e7dac2efc8ec001e
Alex Karev <w88190@gmail.com>
2022-04-23 17:25:22 +0700

    добавить обработчики событий в класс card

--- scripts/script.js
+++ scripts/script.js
@@ -63,7 +63,7 @@ class Card {
         this._cardSelector = cardSelector;
     }
 
-    _getTemplate() {
+    _getTemplate = () => {
         const cardElement = document
             .querySelector(this._cardSelector)
             .content
@@ -72,19 +72,41 @@ class Card {
         return cardElement
     }
 
-    generateCard() {
+    generateCard = () => {
         const cardElement = this._getTemplate();
+        this._addEventListener(cardElement);
         const linkCard = cardElement.querySelector('.elements__image');
         linkCard.src = this._link;
         linkCard.alt = this._title;
         const nameCard = cardElement.querySelector('.elements__title');
         nameCard.textContent = this._title;
-        const trashButton = cardElement.querySelector('.elements__trash');
-        trashButton.addEventListener('click', removeCard);
-        const likeButton = cardElement.querySelector('.elements__like-button');
-        likeButton.addEventListener('click', likeCard);
-        linkCard.addEventListener('click', openZoomPopup);
+
       return cardElement
+    }
+
+    _removeCard = (event) => {
+        event.currentTarget.closest('.elements__item').remove();
+    }
+
+    _likeCard = (event) => {
+        event.currentTarget.classList.toggle('elements__like-button_active');
+    }
+
+    _openZoomPopup = (event) => {
+        const titleCard = event.currentTarget.closest('.elements__item');
+        popupZoomImage.src = event.currentTarget.src;
+        popupZoomImage.alt = event.currentTarget.alt;
+        popupZoomTitle.textContent = titleCard.querySelector('.elements__title').textContent;
+        openPopup(popupZoom);
+    }
+
+    _addEventListener = (cardElement) => {
+        const trashButton = cardElement.querySelector('.elements__trash');
+        trashButton.addEventListener('click', this._removeCard);
+        const likeButton = cardElement.querySelector('.elements__like-button');
+        likeButton.addEventListener('click', this._likeCard);
+        const linkCard = cardElement.querySelector('.elements__image');
+        linkCard.addEventListener('click', this._openZoomPopup);
     }
 }
 
@@ -141,13 +163,7 @@ function openPlacePopup(event) {
     openPopup(popupPlace);
 }
 
-function openZoomPopup(event) {
-    const titleCard = event.currentTarget.closest('.elements__item');
-    popupZoomImage.src = event.currentTarget.src;
-    popupZoomImage.alt = event.currentTarget.alt
-    popupZoomTitle.textContent = titleCard.querySelector('.elements__title').textContent;
-    openPopup(popupZoom);
-}
+
 
 function closePopup(popup) {
     popup.classList.remove('popup_opened');
@@ -183,13 +199,7 @@ function handlePlaceFormSubmit(event) {
     formElementPlace.reset();
 }
 
-function removeCard(event) {
-    event.currentTarget.closest('.elements__item').remove();
-}
 
-function likeCard(event) {
-    event.currentTarget.classList.toggle('elements__like-button_active');
-}
 
 function createCard(link, name) {
     const elementItem = cardTemplate.content.firstElementChild.cloneNode(true);
